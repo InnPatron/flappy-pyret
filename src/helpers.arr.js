a2fc587905d8b9556ef6a689a081d72db79bc1c2
Alexander Vo <randomcivvy0121@gmail.com>
2018-12-11 15:39:51 -0700

    Add helpers.rng() Returns number between (-x, x)

--- src/helpers.arr.js
+++ src/helpers.arr.js
@@ -2,4 +2,8 @@ module.exports = {
   'concat-strings': function(list) {
     return list.join("");
   },
+
+  'rng': function(upperInclusive) {
+    return (Math.random() * 2 * upperInclusive) - upperInclusive;
+  },
 };
